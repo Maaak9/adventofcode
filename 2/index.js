5f4c7dcdea101175358c0e34a7891dbdddc090e5
Marcus Kärrman <marcus.karrman@textalk.sel>
2019-12-03 10:36:10 +0100

    part 2 of day2

--- 2/index.js
+++ 2/index.js
@@ -112,3 +112,54 @@ Once the program has halted, its output is available at address 0, also just lik
 Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
 */
 
+for (let i = 0; i < 10000; i++) {
+  let noun = 0 + Math.floor(i / 100);
+  let verb = i % 100;
+
+  puzzle = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,6,19,23,1,10,23,27,2,27,13,31,1,31,6,35,2,6,35,39,1,39,5,43,1,6,43,47,2,6,47,51,1,51,5,55,2,55,9,59,1,6,59,63,1,9,63,67,1,67,10,71,2,9,71,75,1,6,75,79,1,5,79,83,2,83,10,87,1,87,5,91,1,91,9,95,1,6,95,99,2,99,10,103,1,103,5,107,2,107,6,111,1,111,5,115,1,9,115,119,2,119,10,123,1,6,123,127,2,13,127,131,1,131,6,135,1,135,10,139,1,13,139,143,1,143,13,147,1,5,147,151,1,151,2,155,1,155,5,0,99,2,0,14,0];
+  pointer = 0;
+  puzzle[1] = noun;
+  puzzle[2] = verb;
+
+  fixComputer();
+
+  function fixComputer() {
+    while(pointer < puzzle.length) {
+      const opcode = puzzle[pointer];
+      const pos1 = puzzle[puzzle[pointer + 1]];
+      const pos2 = puzzle[puzzle[pointer + 2]];
+      const outputPos = puzzle[pointer + 3];
+
+      switch(opcode) {
+        case 1: {
+          puzzle[outputPos] =  pos1 + pos2;
+          pointer += 4;
+          break;
+        }
+        case 2: {
+          puzzle[outputPos] = pos1 * pos2;
+          pointer += 4;
+          break;
+        }
+        case 99: {
+          pointer = puzzle.length + 1;
+          break;
+        }
+        default: {
+          console.warn('Error unknown opcode');
+          break;
+        }
+      }
+    }
+  }
+
+  if(puzzle[0] === 19690720) {
+    console.warn(`100 * ${noun} + ${verb} = `, 100 * noun + verb);
+  }
+}
+
+
+
+
+
+
