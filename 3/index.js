aa423676fc1d7fa8a07ac2f4dc4b1b693369b04a
Marcus Kärrman <marcuskarrm@gmail.com>
2019-12-08 12:33:48 +0100

    day3 part 2

--- 3/index.js
+++ 3/index.js
@@ -55,6 +55,7 @@ let currentCord = { x: 0, y: 0 };
 
 function getWireCordinates(wireData) {
   const cords = {};
+  let index = 0;
   wireData.forEach((data) => {
     const direction = data[0];
     const length = parseInt(data.substring(1));
@@ -82,7 +83,12 @@ function getWireCordinates(wireData) {
 
         }
       }
-      cords[`[${currentCord.x}][${currentCord.y}]`] =  'o'
+      index += 1;
+      cords[`[${currentCord.x}][${currentCord.y}]`] =  {
+        x: currentCord.x,
+        y: currentCord.y,
+        index,
+      }
     }
   });
   return cords;
@@ -110,6 +116,7 @@ collidingWireCords = collidingWireCords.map((cords) => {
     x: cords.x,
     y: cords.y,
     distance,
+    totalWireDistance: wire1Cords[`[${cords.x}][${cords.y}]`].index + wire2Cords[`[${cords.x}][${cords.y}]`].index,
   };
 });
 
@@ -117,3 +124,39 @@ console.warn('smallestDistance', smallestDistance);
 // 1983
 
 
+/*
+--- Part Two ---
+It turns out that this circuit is very timing-sensitive; you actually need to minimize the signal delay.
+
+To do this, calculate the number of steps each wire takes to reach each intersection; choose the intersection where the sum of both wires' steps is lowest. If a wire visits a position on the grid multiple times, use the steps value from the first time it visits that position when calculating the total value of a specific intersection.
+
+The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location, including the intersection being considered. Again consider the example from above:
+
+...........
+.+-----+...
+.|.....|...
+.|..+--X-+.
+.|..|..|.|.
+.|.-X--+.|.
+.|..|....|.
+.|.......|.
+.o-------+.
+...........
+In the above example, the intersection closest to the central port is reached after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the second wire for a total of 20+20 = 40 steps.
+
+However, the top-right intersection is better: the first wire takes only 8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+
+Here are the best steps for the extra examples from above:
+
+R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+What is the fewest combined steps the wires must take to reach an intersection?
+*/
+
+// Added index in getWireCordinates and just sort
+collidingWireCords.sort((a,b) => a.totalWireDistance - b.totalWireDistance);
+
+console.warn('Smallest total wire distance :', collidingWireCords[0].totalWireDistance);
+// Answer: 107754
